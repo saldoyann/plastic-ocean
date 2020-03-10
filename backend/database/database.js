b3ce3b824c128c2eea543ea28a321e5bbd34d913
Yann SALDO <yann.saldo@gmail.com>
2020-03-10 20:04:06 +0100

    changed server.js

--- backend/database/database.js
+++ backend/database/database.js
@@ -1,11 +1,19 @@
 const mysql = require('mysql');
 
-const connection =mysql.createConnection({
-    host: 'localhost',
+// const connection =mysql.createConnection({
+//     host: 'localhost',
+//     port: '8889',
+//     user: 'root',
+//     password: 'root',
+//     database: 'climact-projet'
+// })
+
+const connection = mysql.createConnection({
+    host: 'eu-cdbr-west-02.cleardb.net',
     port: '8889',
-    user: 'root',
-    password: 'root',
-    database: 'climact-projet'
+    user: 'b1752824fb5086',
+    password: 'a9a2e190',
+    database: 'heroku_2d70bf9b778a071'
 })
 
 module.exports = connection;
